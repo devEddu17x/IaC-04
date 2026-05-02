2b2824696eb7625b0a2a13d4fb707ce233cc245f
Eddu <azabacheeduardo85@gmail.com>
2026-05-02 15:44:55 -0500

    feat: add endpoints module configuration in main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -10,3 +10,14 @@ module "security" {
   vpc_id = module.networking.vpc_id
   env    = "dev"
 }
+
+module "endpoints" {
+  source                     = "../../modules/endpoints"
+  aws_region                 = var.aws_region
+  vpc_id                     = module.networking.vpc_id
+  route_table_private_a_id   = module.networking.route_table_private_a_id
+  route_table_private_b_id   = module.networking.route_table_private_b_id
+  private_subnet_az_a_id     = module.networking.private_subnet_az_a_id
+  private_subnet_az_b_id     = module.networking.private_subnet_az_b_id
+  security_group_vpce_sqs_id = module.security.sg_sqs_vpce_id
+}
