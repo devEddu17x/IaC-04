463eac086f6cd869835f1b5a7b064236846c1aa5
Eddu <azabacheeduardo85@gmail.com>
2026-05-02 15:45:01 -0500

    feat: add route table outputs for private subnets in outputs.tf

--- environments/dev/outputs.tf
+++ environments/dev/outputs.tf
@@ -12,3 +12,15 @@ output "private_subnet_az_b_id" {
   description = "Main private subnet ID in AZ-b"
   value       = module.networking.private_subnet_az_b_id
 }
+
+output "route_table_private_a_id" {
+  description = "Route table for private subnet in AZ-a"
+  value       = module.networking.route_table_private_a_id
+
+}
+
+output "route_table_private_b_id" {
+  description = "Route table for private subnet in AZ-b"
+  value       = module.networking.route_table_private_b_id
+
+}
