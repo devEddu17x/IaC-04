e9e064de78d9de73e2f81051c8926d8b4889a1f2
Eddu <azabacheeduardo85@gmail.com>
2026-05-02 15:44:00 -0500

    fix: update security group resource naming conventions in rules.tf

--- modules/security/rules.tf
+++ modules/security/rules.tf
@@ -3,8 +3,8 @@ resource "aws_security_group_rule" "sqs_inbound_sign_lambda" {
   from_port                = 443
   to_port                  = 443
   protocol                 = "tcp"
-  security_group_id        = aws_security_group.sg-sqs_vpce.id
-  source_security_group_id = aws_security_group.sg-sign_lambda.id
+  security_group_id        = aws_security_group.sg_sqs_vpce.id
+  source_security_group_id = aws_security_group.sg_sign_lambda.id
 }
 
 resource "aws_security_group_rule" "sqs_inbound_crop_lambda" {
@@ -12,8 +12,8 @@ resource "aws_security_group_rule" "sqs_inbound_crop_lambda" {
   from_port                = 443
   to_port                  = 443
   protocol                 = "tcp"
-  security_group_id        = aws_security_group.sg-sqs_vpce.id
-  source_security_group_id = aws_security_group.sg-crop_lambda.id
+  security_group_id        = aws_security_group.sg_sqs_vpce.id
+  source_security_group_id = aws_security_group.sg_crop_lambda.id
 }
 
 resource "aws_security_group_rule" "sign_lambda_outbound" {
@@ -21,8 +21,8 @@ resource "aws_security_group_rule" "sign_lambda_outbound" {
   from_port                = 443
   to_port                  = 443
   protocol                 = "tcp"
-  security_group_id        = aws_security_group.sg-sign_lambda.id
-  source_security_group_id = aws_security_group.sg-sqs_vpce.id
+  security_group_id        = aws_security_group.sg_sign_lambda.id
+  source_security_group_id = aws_security_group.sg_sqs_vpce.id
 }
 
 resource "aws_security_group_rule" "crop_lambda_outbound" {
@@ -30,6 +30,6 @@ resource "aws_security_group_rule" "crop_lambda_outbound" {
   from_port                = 443
   to_port                  = 443
   protocol                 = "tcp"
-  security_group_id        = aws_security_group.sg-crop_lambda.id
-  source_security_group_id = aws_security_group.sg-sqs_vpce.id
+  security_group_id        = aws_security_group.sg_crop_lambda.id
+  source_security_group_id = aws_security_group.sg_sqs_vpce.id
 }
