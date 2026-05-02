1ca12b06a239a50578793e2a7d06bf82b09c5a9c
Eddu <azabacheeduardo85@gmail.com>
2026-05-02 15:44:19 -0500

    fix: add missing route table outputs for private subnets in outputs.tf

--- modules/networking/outputs.tf
+++ modules/networking/outputs.tf
@@ -12,3 +12,15 @@ output "private_subnet_az_b_id" {
   description = "Main private subnet ID in AZ-b"
   value       = aws_subnet.private-AZ-b.id
 }
+
+output "route_table_private_a_id" {
+  description = "Route table for private subnet in AZ-a"
+  value       = aws_route_table.private_a.id
+
+}
+
+output "route_table_private_b_id" {
+  description = "Route table for private subnet in AZ-b"
+  value       = aws_route_table.private_b.id
+
+}
