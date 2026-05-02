9bc23ae7db8d5634bc31f34faa5625755bdff9b2
Eddu <azabacheeduardo85@gmail.com>
2026-05-02 15:44:05 -0500

    fix: correct output naming conventions in outputs.tf

--- modules/security/outputs.tf
+++ modules/security/outputs.tf
@@ -1,9 +1,14 @@
-output "sg_sign_lambda-id" {
+output "sg_sign_lambda_id" {
   description = "Security group id from sign_lambda"
-  value       = aws_security_group.sg-sign_lambda.id
+  value       = aws_security_group.sg_sign_lambda.id
 }
 
-output "sg_crop_lambda-id" {
+output "sg_crop_lambda_id" {
   description = "Security group id from crop_lambda"
-  value       = aws_security_group.sg-crop_lambda.id
+  value       = aws_security_group.sg_crop_lambda.id
+}
+
+output "sg_sqs_vpce_id" {
+  description = "Security group id from sqs vpce"
+  value       = aws_security_group.sg_sqs_vpce.id
 }
