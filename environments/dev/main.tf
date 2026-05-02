2985fafa8ad16efa1bddec4b3dd6116f0c621b98
Eddu <azabacheeduardo85@gmail.com>
2026-05-02 14:50:57 -0500

    feat: implement networking module in development environment

---
+++ environments/dev/main.tf
@@ -0,0 +1,6 @@
+module "networking" {
+  source                        = "../../modules/networking"
+  aws_region                    = var.aws_region
+  aws_availability_zone_main    = var.aws_availability_zone_main
+  aws_availability_zone_replica = var.aws_availability_zone_replica
+}
