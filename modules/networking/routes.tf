9ce84797013e20914cacac79996e1f6e6110266a
Eddu <azabacheeduardo85@gmail.com>
2026-05-02 14:38:27 -0500

    feat: add routing configurations to networking module

---
+++ modules/networking/routes.tf
@@ -0,0 +1,18 @@
+resource "aws_route" "public_internet_access" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.gw.id
+}
+
+resource "aws_route" "private_a_nat" {
+  route_table_id         = aws_route_table.private_a.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat_a.id
+}
+
+
+resource "aws_route" "private_b_nat" {
+  route_table_id         = aws_route_table.private_b.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat_b.id
+}
