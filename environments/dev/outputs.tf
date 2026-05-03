930b1c779a29a627e16e7f15105c4318b1afc0a9
Eddu <azabacheeduardo85@gmail.com>
2026-05-03 01:28:17 -0500

    feat: replace networking outputs with API invoke URL in outputs.tf

--- environments/dev/outputs.tf
+++ environments/dev/outputs.tf
@@ -1,26 +1,5 @@
-output "networking_vpc_id" {
-  description = "VPC id"
-  value       = module.networking.vpc_id
+output "api_invoke_url" {
+  description = "Base invoke URL for the deployed API stage"
+  value       = module.apigateway.api_invoke_url
 }
 
-output "private_subnet_az_a_id" {
-  description = "Main private subnet ID in AZ-a"
-  value       = module.networking.private_subnet_az_a_id
-}
-
-output "private_subnet_az_b_id" {
-  description = "Main private subnet ID in AZ-b"
-  value       = module.networking.private_subnet_az_b_id
-}
-
-output "route_table_private_a_id" {
-  description = "Route table for private subnet in AZ-a"
-  value       = module.networking.route_table_private_a_id
-
-}
-
-output "route_table_private_b_id" {
-  description = "Route table for private subnet in AZ-b"
-  value       = module.networking.route_table_private_b_id
-
-}
