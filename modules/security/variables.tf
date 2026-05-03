10dc0c691cc1ab06453e8ad04d31c4e6dea6e032
Eddu <azabacheeduardo85@gmail.com>
2026-05-03 01:27:21 -0500

    feat: include aws_region variable in security group module

--- modules/security/variables.tf
+++ modules/security/variables.tf
@@ -3,6 +3,11 @@ variable "vpc_id" {
   type        = string
 }
 
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+}
+
 variable "env" {
   description = "Environment name (dev, qa, prod)"
   type        = string
