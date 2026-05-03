dcb638c6e0f86f99127f665d16acce4bb6341175
Eddu <azabacheeduardo85@gmail.com>
2026-05-03 14:56:52 -0500

    feat: update IAM policy names to include environment variable

--- modules/iam/iam_policy.tf
+++ modules/iam/iam_policy.tf
@@ -1,14 +1,14 @@
 resource "aws_iam_policy" "sign_s3_policy" {
-  name   = "sign-lambda-s3-policy"
+  name   = "sign-lambda-s3-policy-${var.env}"
   policy = data.aws_iam_policy_document.sign_s3_permissions.json
 }
 
 resource "aws_iam_policy" "crop_s3_policy" {
-  name   = "crop-lambda-s3-policy"
+  name   = "crop-lambda-s3-policy-${var.env}"
   policy = data.aws_iam_policy_document.crop_s3_permissions.json
 }
 
 resource "aws_iam_policy" "crop_sqs_policy" {
-  name   = "crop-lambda-sqs-policy"
+  name   = "crop-lambda-sqs-policy-${var.env}"
   policy = data.aws_iam_policy_document.crop_sqs_permissions.json
 }
