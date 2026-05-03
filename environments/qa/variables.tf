cd4e2b97ac3f3d0c44c0e455cbbdddb7e4eaf436
Eddu <azabacheeduardo85@gmail.com>
2026-05-03 14:45:31 -0500

    feat: add variables for AWS configuration in QA environment

---
+++ environments/qa/variables.tf
@@ -0,0 +1,25 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+}
+variable "aws_profile" {
+  description = "AWS profile"
+  type        = string
+}
+variable "env" {
+  description = "Environment: dev, qa, prod"
+  type        = string
+}
+variable "name_prefix" {
+  description = "Prefix for unique aws resource names"
+  type        = string
+  default     = "eddu"
+}
+variable "aws_availability_zone_main" {
+  description = "AWS main availability zone"
+  type        = string
+}
+variable "aws_availability_zone_replica" {
+  description = "AWS replica availability zone"
+  type        = string
+}
