2afe107a3190ac05acee9389cceedb972cd16080
Eddu <azabacheeduardo85@gmail.com>
2026-05-03 19:16:58 -0500

    refactor: remove S3 to SQS policy to simplify configuration

--- modules/sqs/policy.tf
+++ modules/sqs/policy.tf
@@ -5,27 +5,3 @@ resource "aws_sqs_queue_redrive_policy" "dead_letter_queue_policy" {
     maxReceiveCount     = 3
   })
 }
-
-data "aws_iam_policy_document" "s3_to_sqs_policy_document" {
-  statement {
-    effect = "Allow"
-
-    principals {
-      type        = "Service"
-      identifiers = ["s3.amazonaws.com"]
-    }
-    actions   = ["sqs:SendMessage"]
-    resources = [aws_sqs_queue.main_queue.arn]
-
-    condition {
-      test     = "ArnEquals"
-      variable = "aws:SourceArn"
-      values   = [var.s3_bucket_images_arn]
-    }
-  }
-}
-
-resource "aws_sqs_queue_policy" "s3_to_sqs_policy" {
-  queue_url = aws_sqs_queue.main_queue.id
-  policy    = data.aws_iam_policy_document.s3_to_sqs_policy_document.json
-}
