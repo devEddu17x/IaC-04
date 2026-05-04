e61d1f89bcc3e5fe695a1cde8fd51ebf48b7c009
Eddu <azabacheeduardo85@gmail.com>
2026-05-03 19:28:24 -0500

    feat: add environment variable to networking module in dev, prod, and qa configurations

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -3,6 +3,7 @@ module "networking" {
   aws_region                    = var.aws_region
   aws_availability_zone_main    = var.aws_availability_zone_main
   aws_availability_zone_replica = var.aws_availability_zone_replica
+  env                           = var.env
 }
 
 module "security" {
