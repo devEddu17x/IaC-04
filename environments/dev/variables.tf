bc788a24dbc2471472668fa3307c352d8da397f5
Eddu <azabacheeduardo85@gmail.com>
2026-05-03 19:55:40 -0500

    feat: remove default value for name_prefix variable in dev, prod, and qa environments

--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -14,7 +14,6 @@ variable "env" {
 variable "name_prefix" {
   description = "Prefix for unique aws resource names"
   type        = string
-  default     = "eddu"
 }
 variable "aws_availability_zone_main" {
   description = "AWS main availability zone"
